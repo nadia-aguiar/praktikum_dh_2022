5fce454756bcd28abbb85487fa146369e2863e02
nadia-aguiar <nadia.aguiar@hotmail.com>
2022-09-06 13:50:26 +0200

    file updated

--- main.py
+++ main.py
@@ -7,27 +7,41 @@ class Aceno:
     
         self.url = "https://periodicoscientificos.ufmt.br/ojs/index.php/aceno/issue/archive"
         self.box = ".page" #CSS Selector
-        #self.box_two = "issues_archive" #class
-        #self.box_article = "li" #CSS Selector 
         self.box_texts = "obj_issue_summary" #class
         self.box_title = "title" # class
+
+        self.page_articles = "page.page_issue" #class
+        self.box_articles = "sections" #class
+        self.list_articles = " div.section:nth-child(2)" #CSS SELECTOR    
+        self.article_title_id = "[id^='article-']" #CSS Selector
 
     def navegate (self):
         self.driver.get(self.url)
 
-    def search(self): 
+    def open_journal(self): 
         element_box =  self.driver.find_element(By.CSS_SELECTOR, self.box)
-        #element_box_two = element_box.find_element(By.CLASS_NAME, self.box_two)
-        #element_box_articles = element_box.find_elements(By.TAG_NAME, self.box_article)
         element_box_texts = element_box.find_element(By.CLASS_NAME, self.box_texts)
         element_title_click = element_box_texts.find_element(By.CLASS_NAME, self.box_title)
         element_title_click.click()
 
-        
+    def open_text(self):
+        element_page_articles = self.driver.find_element(By.CLASS_NAME, self.page_articles)
+        element_box_articles = element_page_articles.find_element(By.CLASS_NAME, self.box_articles)            
+        elelemts_list_articles = element_box_articles.find_element(By.CSS_SELECTOR, self.list_articles)
+        element_article_title_id = elelemts_list_articles.find_element(By.CSS_SELECTOR, self.article_title_id)
+        element_article_title_id.click()  
+
+    #def get_text_information(self):     
+
+
+
         
         
 ff = webdriver.Firefox()
 a = Aceno(ff) 
 a.navegate()  
-a.search()  
+a.open_journal() 
+a.open_text() 
 ff.quit() 
+
+
