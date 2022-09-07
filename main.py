5910a4ea8281320c62a6f9ec00320b9fd6145a92
nadia-aguiar <nadia.aguiar@hotmail.com>
2022-09-07 18:15:31 +0200

    update

--- main.py
+++ main.py
@@ -24,17 +24,19 @@ class Aceno:
         self.author_text = "authors" #class
         self.doi_text = "item.doi" #class
         self.abstract_text = "item.abstract" #class
+        self.return_page_article = "/html/body/div[1]/div[1]/div[1]/div/article/div/div[2]/div[4]/section[1]/div/a" #XPATH
 
 
 
     def navegate (self):
         self.driver.get(self.url)
 
-    def open_journal(self): 
+    def open_journal(self):
         element_all_journals =  self.driver.find_element(By.CSS_SELECTOR, self.all_journals)
         element_journal = element_all_journals.find_element(By.CLASS_NAME, self.journal)
         element_date_journal_click = element_journal.find_element(By.CLASS_NAME, self.date_journal)
         element_date_journal_click.click()
+        
 
     def open_text(self):
         element_page_articles = self.driver.find_element(By.CLASS_NAME, self.page_articles)
@@ -48,58 +50,59 @@ class Aceno:
         try:
             element_number_journal = element_box_info_text.find_element(By.CSS_SELECTOR, self.number_journal).text 
         except NoSuchElementException:
-            pass  
+            element_number_journal = "None"  
         try:
             element_author_text = element_box_info_text.find_element(By.CLASS_NAME, self.author_text).text
         except NoSuchElementException:
-            pass
+            element_author_text = "None"
         try:
             element_doi_text = element_box_info_text.find_element(By.CLASS_NAME, self.doi_text).text
         except NoSuchElementException:
-            pass
+            element_doi_text = "None"
         try:
             element_abstract_text = element_box_info_text.find_element(By.CLASS_NAME, self.abstract_text).text
         except NoSuchElementException:
-            pass
-
+            element_abstract_text = "None"
         list_text_information = [element_number_journal, element_author_text, element_doi_text, element_abstract_text]
         return list_text_information
         
-                
-         
 
     def save_text_information(self):
         text_information = self.get_text_information()
-        print(text_information)
         if not os.path.exists("Aceno"):
             os.makedirs("Aceno")
         with open("aceno_texts.csv", "w", newline='') as file:
             writer = csv.writer(file)            
             writer.writerow(text_information)
-            writer.writerow()
+            writer.writerow("")
 
     def to_iterate(self):
-        open_one_journal = self.open_journal()
-        open_one_text = self.open_text()
-        get_one_text = self.get_text_information()
-        save_file =self.save_text_information()
-        for i in range(len(self.element_journal)):
-            for i in range(len(self.element_article_title_id)):
-                open_one_journal()
-                open_one_text()
-                get_one_text()
-                save_file()
+        count= 0
+        numerador = 0
+        self.navegate()
+        for idx in range(len(self.journal)):
+            self.open_journal()
+            self.open_text()
+            for idx in range(len(self.article_title_id)):
+                self.get_text_information()
+                self.save_text_information()
+                element_return_page_article = self.driver.find_element(By.XPATH, self.return_page_article)
+                element_return_page_article.click()
+                self.open_text()
+                count+=1
+                self.open_journal()
+        numerador+=1
         print("It's done!")
 
 
         
 ff = webdriver.Firefox()
 a = Aceno(ff) 
-a.navegate()  
-a.open_journal() 
-a.open_text() 
-a.get_text_information()
-a.save_text_information()
+#a.navegate()  
+#a.open_journal() 
+#a.open_text() 
+#a.get_text_information()
+#a.save_text_information()
 a.to_iterate()
 ff.quit() 
 
