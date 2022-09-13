64a5d6b3ba3bcf9d7a5aa4e95f9e3e202cc4f97a
nadia-aguiar <nadia.aguiar@hotmail.com>
2022-09-13 20:17:34 +0200

    function click_next_page added

--- main.py
+++ main.py
@@ -18,13 +18,10 @@ class Aceno:
         self.driver = driver
     
         self.url = "https://periodicoscientificos.ufmt.br/ojs/index.php/aceno/issue/archive"
-        #self.all_issues = ".issues_archive" #CSS Selector 
         self.issue = "obj_issue_summary" #class
         self.date_issue = "title" # class
         self.issue_link = "href" # attribute
 
-        #self.page_articles = "page.page_issue" #class
-        #self.box_articles = "sections" #class
         self.list_articles = "obj_article_summary"# class  
         self.article_title_id = "[id^='article-']" #CSS Selector
         self.article_link = "href" #attribute
@@ -33,8 +30,12 @@ class Aceno:
         self.number_issue = "a.title" #CSS Selector
         self.author_text = "authors" #class
         self.doi_text = "item.doi" #class
-        self.abstract_text = "section.item:nth-child(3)"#html body.pkp_page_article.pkp_op_view div.pkp_structure_page div.pkp_structure_content.has_sidebar div.pkp_structure_main div.page.page_article article.obj_article_details div.row div.main_entry section.item.abstract p" #CSS Selector
+        self.abstract_text = "html body.pkp_page_article.pkp_op_view div.pkp_structure_page div.pkp_structure_content.has_sidebar div.pkp_structure_main div.page.page_article article.obj_article_details div.row div.main_entry section.item.abstract p" #CSS Selector
+        self.abstract_title = "page_title" #class
+
         self.return_page_article = "/html/body/div[1]/div[1]/div[1]/div/article/div/div[2]/div[4]/section[1]/div/a" #XPATH
+        self.box_next_page = "/html/body/div/div[1]/div[1]/div/div" #XPATH
+        self.next_page = "next" #class
 
     """function to open the link of the journal Aceno"""
     def navegate (self):
@@ -43,7 +44,6 @@ class Aceno:
 
     """function to get list of all issue links and return a list"""
     def get_all_issues(self):
-        #element_issues_box = self.driver.find_element(By.CSS_SELECTOR, self.all_issues)
         element_all_issues = self.driver.find_elements(By.CLASS_NAME, self.issue)
         link_list = []
         for element_issue in element_all_issues:
@@ -61,8 +61,6 @@ class Aceno:
 
     """function to get all article links and return a list"""
     def get_all_articles(self):
-        #element_page_articles = self.driver.find_element(By.CLASS_NAME, self.page_articles)
-        #element_box_articles = element_page_articles.find_element(By.CLASS_NAME, self.box_articles)            
         elements_list_articles = self.driver.find_elements(By.CLASS_NAME, self.list_articles)
         link_list = []
         for element_list_article in elements_list_articles:
@@ -91,8 +89,15 @@ class Aceno:
             element_abstract_text = element_box_info_text.find_element(By.CSS_SELECTOR, self.abstract_text).text
         except NoSuchElementException:
             element_abstract_text = "  Abstract is not available."
-        text_information = [element_number_issue, element_author_text, element_doi_text, element_abstract_text]
+        try:
+            element_abstract_title = element_box_info_text.find_element (By.CLASS_NAME, self.abstract_title).text
+        except NoSuchElementException:
+            element_abstract_title = "Title is not available."
+        
+        text_information = [element_number_issue, element_author_text, element_doi_text, element_abstract_title, element_abstract_text]
         return text_information
+
+        
 
 
 
@@ -110,6 +115,20 @@ class Aceno:
             file.close()
 
 
+
+    """function to go to the next page, if one exists"""
+    def click_next_page (self):       
+        try:
+            element_box_next_page = self.driver.find_element(By.XPATH, self.box_next_page) 
+            element_next_page = element_box_next_page.find_element(By.CLASS_NAME, self.next_page)
+            element_next_page.click()
+            self.url = self.driver.current_url #2
+            self.to_iterate()
+        except NoSuchElementException:
+            print("That's all folks!")
+
+
+
     """function to iterate overall issues and articles"""
     def to_iterate(self):
         self.navegate()
@@ -121,7 +140,8 @@ class Aceno:
             for idx in range(len(link_articles)):
                 self.driver.get(link_articles[idx])
                 self.save_text_information()
-        print("It's done!")
+        self.driver.get(self.url)
+        self.click_next_page()
         
 
     """function to report the moment of the search and return a txt"""
@@ -135,6 +155,7 @@ class Aceno:
         with open(os.path.join(dir,file_name), "a", newline='') as file:
             file.write (f"This search was made in {current_time}\n")
             file.close()
+    
         
 ff = webdriver.Firefox()
 a = Aceno(ff) 
